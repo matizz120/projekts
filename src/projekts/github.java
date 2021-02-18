be3b1f193e8e04bdfa1b780af6ce283d7237ff13
Matīss Petrausks <matiss44555@gmail.com>
2021-02-18 18:14:46 +0200

    Projektu uzsākšana

---
+++ src/projekts/github.java
@@ -0,0 +1,12 @@
+package projekts;
+
+public class github {
+
+	public static void main(String[] args) {
+		System.out.print("Jânis");
+		int x = 4, y = 6;
+		System.out.print(" x + y = "+(x+y));
+
+	}
+
+}
